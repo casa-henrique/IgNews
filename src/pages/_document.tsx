1da5ff5b22418492f7889262d05aebee4cb810ce
casa-henrique <riquecasa@gmail.com>
2022-03-23 18:47:37 -0300

    _document e fonte externa

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+
+          <title>IgNews</title>
+        </Head>
+        <body>
+          <Main /> {/*todo nossa aplicação será renderizada nesse main*/}
+          <NextScript />
+          {/*onde o next vai colocar os arquivos js que a
+          aplicação precisa para funcionar*/}
+        </body>
+      </Html>
+    );
+  }
+}
