c003d5a914a1c7bac1ba3629934fa58e350a5380
casa-henrique <riquecasa@gmail.com>
2022-03-24 14:56:37 -0300

    Página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
         {/*Componente react que anexa dados ao head do _document. Importante para SEO*/}
-        <title>Inicio | Ig.News</title>
+        <title>Home | Ig.News</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
