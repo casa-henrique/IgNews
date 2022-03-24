d7a27872db2eba9e2fbd7f336e30508b7ba83d07
casa-henrique <riquecasa@gmail.com>
2022-03-24 18:15:04 -0300

    Consumindo a API da Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 
 import { SubscribeButton } from "../components/SubscribeButton";
 
+import { stripe } from "../services/stripe";
+
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: { priceId: string; amount: number };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,12 +26,28 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KgwbzGtLPzSpS2eDV1n6o21");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product }, //Tudo que passarmos como props nós poderemos acessar nas props da função home neste caso
+  };
+};
